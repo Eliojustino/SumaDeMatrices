dc2b421c2cb547f3fb9c98d8a0cefcf671fb576e
ELIO <eliojustinoc@gmail.com>
2023-03-08 08:44:07 -0600

    Hice las funciones para la inpresión de los números

---
+++ src/dev/LecturaDeNumeros.java
@@ -0,0 +1,11 @@
+package dev;
+
+public class LecturaDeNumeros {
+	public static void main(String[] args) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].lengtj; j++) {
+				System.out.print(m[i]);
+			}
+		}
+	}
+}
